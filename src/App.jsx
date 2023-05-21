ffcb841f24c7bcb2c963823aca82c66ed72ee0b3
Yash <yashlotan7@gmail.com>
2023-05-21 19:16:13 +0530

    Added hyperlinks to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { FullPage, Slide } from 'react-full-page'
 import SlideControls from './common/SlideControls'
 import Navbar from './common/Navbar'
+import { useRef } from 'react'
 
 const App = () => {
+    const fullPageRef = useRef(null)
+
+    const scrollToSection = index => {
+        fullPageRef.current.scrollToSlide(index)
+    }
+
     return (
         <div>
-            <Navbar />
+            <Navbar scrollToSection={scrollToSection} />
             <div className="max-w-screen-2xl mx-auto pt-16">
-                <FullPage controls={SlideControls} className="max-w-screen-2xl">
+                <FullPage
+                    controls={SlideControls}
+                    className="max-w-screen-2xl"
+                    ref={fullPageRef}
+                >
                     <Slide className="flex items-center">
                         <h1 className="text-8xl w-3/4">
                             A Full-Stack Ninja who can Tackle any Web
--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
-const Navbar = () => {
+const Navbar = ({ scrollToSection }) => {
     return (
         <div className="navbar-wrapper fixed w-full shadow-xl bg-secondary">
             <div className="max-w-screen-2xl mx-auto flex justify-between py-3">
@@ -8,7 +8,39 @@ const Navbar = () => {
                     <img src="/logo.svg" className="w-10" />
                 </div>
                 <div className="flex items-center">
-                    <div className="cursor-pointer text-2xl">
+                    <div
+                        className="cursor-pointer text-sm uppercase"
+                        onClick={() => {
+                            scrollToSection(1)
+                        }}
+                    >
+                        About
+                    </div>
+                    <div
+                        className="cursor-pointer ml-8 text-sm uppercase"
+                        onClick={() => {
+                            scrollToSection(2)
+                        }}
+                    >
+                        Work
+                    </div>
+                    <div
+                        className="cursor-pointer ml-8 text-sm uppercase"
+                        onClick={() => {
+                            scrollToSection(3)
+                        }}
+                    >
+                        Skills
+                    </div>
+                    <div
+                        className="cursor-pointer ml-8 text-sm uppercase"
+                        onClick={() => {
+                            scrollToSection(4)
+                        }}
+                    >
+                        Contact
+                    </div>
+                    <div className="cursor-pointer text-2xl ml-8">
                         <a href="https://github.com/yash0212" target="_blank">
                             <FaGithub />
                         </a>
