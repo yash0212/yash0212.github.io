beb56361fe88e1f53c778bed970cf74460ac097e
Yash <yashlotan7@gmail.com>
2023-05-21 19:07:14 +0530

    Added intro

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ const App = () => {
             <Navbar />
             <div className="max-w-screen-2xl mx-auto pt-16">
                 <FullPage controls={SlideControls} className="max-w-screen-2xl">
-                    <Slide>
-                        <h1>Intro</h1>
+                    <Slide className="flex items-center">
+                        <h1 className="text-8xl w-3/4">
+                            A Full-Stack Ninja who can Tackle any Web
+                            Development Challenge with Skill and Flair
+                        </h1>
                     </Slide>
                     <Slide>
                         <h1>About</h1>
