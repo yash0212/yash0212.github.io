0486fdfdb65713bac755409278e4a2633606b688
Yash <yashlotan7@gmail.com>
2023-05-22 00:09:12 +0530

    Added about section

--- src/App.jsx
+++ src/App.jsx
@@ -20,26 +20,23 @@ const App = () => {
                     ref={fullPageRef}
                 >
                     <Slide className="flex items-center">
-                        <h2 className="text-8xl w-3/4">
+                        <h2 className="text-8xl w-3/4 leading-[1.125]">
                             A Full-Stack Ninja who can Tackle any Web
                             Development Challenge with Skill and Flair
                         </h2>
                     </Slide>
-                    <Slide className="flex items-center">
-                        <h2 className="text-8xl w-3/4">About</h2>
-                        <div className="">
-                            I’m a full-stack developer with over three years of
-                            experience in building web applications for ed-tech
-                            and e-commerce startups. I have worked on multiple
-                            products using the MERN stack and cloud services
-                            such as AWS and GCP
-                        </div>
-                        <div className="">
-                            I’m a MERN stack expert who has created innovative
-                            and user-friendly web solutions for education and
-                            commerce. I have worked with various startups and
-                            delivered multiple products using AWS, GCP, GitHub
-                            and Bitbucket
+                    <Slide className="flex justify-center flex-col">
+                        <h2 className="text-6xl w-3/4 font-semibold">About</h2>
+                        <div className="mt-8 text-xl max-w-4xl leading-relaxed">
+                            Hi, I’m Yash Lotan, a full stack developer who loves
+                            to code and create. I have over three years of
+                            experience in ed-tech and ecommerce startups, where
+                            I have built and launched multiple web products
+                            using the MERN stack. I am skilled in using AWS and
+                            GCP for cloud computing and GitHub and Bitbucket for
+                            versioning and teamwork. I am always eager to learn
+                            new technologies and explore new challenges in web
+                            development.
                         </div>
                     </Slide>
                     <Slide>
