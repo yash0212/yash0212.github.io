a47413412ba86561e8b746a112145f633ce660ca
Yash <yashlotan7@gmail.com>
2023-05-21 22:24:24 +0530

    Added about content

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,36 @@ const App = () => {
                     ref={fullPageRef}
                 >
                     <Slide className="flex items-center">
-                        <h1 className="text-8xl w-3/4">
+                        <h2 className="text-8xl w-3/4">
                             A Full-Stack Ninja who can Tackle any Web
                             Development Challenge with Skill and Flair
-                        </h1>
+                        </h2>
+                    </Slide>
+                    <Slide className="flex items-center">
+                        <h2 className="text-8xl w-3/4">About</h2>
+                        <div className="">
+                            I’m a full-stack developer with over three years of
+                            experience in building web applications for ed-tech
+                            and e-commerce startups. I have worked on multiple
+                            products using the MERN stack and cloud services
+                            such as AWS and GCP
+                        </div>
+                        <div className="">
+                            I’m a MERN stack expert who has created innovative
+                            and user-friendly web solutions for education and
+                            commerce. I have worked with various startups and
+                            delivered multiple products using AWS, GCP, GitHub
+                            and Bitbucket
+                        </div>
                     </Slide>
                     <Slide>
-                        <h1>About</h1>
+                        <h2>Work</h2>
                     </Slide>
                     <Slide>
-                        <h1>Work</h1>
+                        <h2>Skills</h2>
                     </Slide>
                     <Slide>
-                        <h1>Skills</h1>
-                    </Slide>
-                    <Slide>
-                        <h1>Contact</h1>
+                        <h2>Contact</h2>
                     </Slide>
                 </FullPage>
             </div>
